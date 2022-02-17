8b5c341677373bbbc0f3e53dd00be68e3702c567
Hannah Dalwai <hannahdalwai4@gmail.com>
2022-02-17 12:03:18 +0200

    first commit

---
+++ frontend-signin/src/router/index.js
@@ -0,0 +1,73 @@
+import { createWebHistory, createRouter } from "vue-router";
+import Home from "@/views/Home.vue";
+import Login from "@/views/Login.vue";
+import Register from "@/views/Register.vue";
+// lazy-loaded-only load it when you need it
+const Profile = () => import("@/views/Profile.vue")
+const BoardAdmin = () => import("@/components/BoardAdmin.vue")
+const BoardModerator = () => import("@/components/BoardModerator.vue")
+const BoardUser = () => import("@/components/BoardUser.vue")
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: Home,
+  },
+  {
+    path: "/home",
+    component: Home,
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: Login,
+  },
+  {
+    path: "/register",
+    name: "eegister",
+    component: Register,
+  },
+  {
+    path: "/profile",
+    name: "profile",
+    // lazy-loaded
+    component: Profile,
+  },
+  {
+    path: "/admin",
+    name: "admin",
+    // lazy-loaded
+    component: BoardAdmin,
+  },
+  {
+    path: "/mod",
+    name: "moderator",
+    // lazy-loaded
+    component: BoardModerator,
+  },
+  {
+    path: "/user",
+    name: "user",
+    // lazy-loaded
+    component: BoardUser,
+  },
+];
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const publicPages = ['/login', '/register', '/home'];
+  const authRequired = !publicPages.includes(to.path);
+  const loggedIn = localStorage.getItem('user');
+  // trying to access a restricted page + not logged in
+  // redirect to login page
+  if (authRequired && !loggedIn) {
+    next('/login');
+  } else {
+    next();
+  }
+});
+
+export default router;
